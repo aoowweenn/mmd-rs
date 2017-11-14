c923b0367f2869d515f545fba1ea5727e0023706
aoowweenn <aoowweenn@gmail.com>
2017-11-14 16:23:20 +0800

    need new string type and type conversion

--- src/pmx.rs
+++ src/pmx.rs
@@ -2,11 +2,10 @@
 // https://gist.github.com/ulrikdamm/8274171
 // https://gist.github.com/felixjones/f8a06bd48f9da9a4539f
 // https://github.com/benikabocha/saba
-use nom::{le_f32, le_u8, le_i8, le_i16, le_i32};
-use nom::{IResult, IError};
-use nom::IResult::{Done, Incomplete};
+use nom::{le_f32, le_i16, le_i32, le_i8, le_u8};
+use nom::IResult;
 use byteorder::{LittleEndian, ReadBytesExt};
-use encoding::{Encoding, DecoderTrap};
+use encoding::{DecoderTrap, Encoding};
 use encoding::all::UTF_16LE;
 use types::{Vec2, Vec3, Vec4};
 use traits::Parse;
@@ -25,21 +24,21 @@ struct Header {
     globals: Globals,
     model_name: String,
     model_name_en: String,
-    comments: String,
-    comments_en: String,
+    comment: String,
+    comment_en: String,
 }
 
 impl Header {
     named!(parse<&[u8], Header>, do_parse!(
         tag!("PMX ") >>
         version: le_f32 >>
-        globals: map!(length_bytes!(le_u8), Globals::from_slice) >>
+        globals: map!(length_bytes!(le_u8), Globals::from) >>
         text: count!(call!(length_string,globals.encoding), 4) >>
 
         ({
             let mut text = text;
-            let comments_en = text.pop().unwrap();
-            let comments = text.pop().unwrap();
+            let comment_en = text.pop().unwrap();
+            let comment = text.pop().unwrap();
             let model_name_en = text.pop().unwrap();
             let model_name = text.pop().unwrap();
             Header {
@@ -47,8 +46,8 @@ impl Header {
                 globals,
                 model_name,
                 model_name_en,
-                comments,
-                comments_en,
+                comment,
+                comment_en,
             }
         })
     ));
@@ -67,7 +66,7 @@ struct Globals {
 }
 
 impl Globals {
-    fn from_slice(input: &[u8]) -> Globals {
+    fn from(input: &[u8]) -> Globals {
         Globals {
             encoding: input[0],
             additional: input[1],
@@ -89,22 +88,29 @@ struct BoneIndexWeight<T> {
 
 impl BoneIndexWeight<i32> {
     fn parse(input: &[u8], index_num_bytes: usize, need_weight: bool) -> IResult<&[u8], Self> {
-        let index_res = {match index_num_bytes {
-            1 => le_i8(input).map(|o| o as i32),
-            2 => le_i16(input).map(|o| o as i32),
-            4 => le_i32(input),
-            _ => unreachable!()
-        }};
+        let index_res = {
+            match index_num_bytes {
+                1 => le_i8(input).map(|o| o as i32),
+                2 => le_i16(input).map(|o| o as i32),
+                4 => le_i32(input),
+                _ => unreachable!(),
+            }
+        };
 
         if !need_weight {
-            return index_res.map(|o| Self {index: o, weight: 1.0f32});
+            return index_res.map(|o| {
+                Self {
+                    index: o,
+                    weight: 1.0f32,
+                }
+            });
         }
 
         let (_, index) = index_res.unwrap();
 
         let weight_raw = &input[index_num_bytes..];
 
-        le_f32(weight_raw).map(|weight| Self {index, weight})
+        le_f32(weight_raw).map(|weight| Self { index, weight })
     }
 }
 
@@ -113,47 +119,43 @@ enum WeightDeform<T> {
     BDEF1 { bones: [BoneIndexWeight<T>; 1] },
     BDEF2 { bones: [BoneIndexWeight<T>; 2] },
     BDEF4 { bones: [BoneIndexWeight<T>; 4] },
-    SDEF  { bones: [BoneIndexWeight<T>; 2], c: Vec3, r0: Vec3, r1: Vec3 },
-    QDEF  { bones: [BoneIndexWeight<T>; 4] },
+    SDEF {
+        bones: [BoneIndexWeight<T>; 2],
+        c: Vec3,
+        r0: Vec3,
+        r1: Vec3,
+    },
+    QDEF { bones: [BoneIndexWeight<T>; 4] },
 }
 
 impl WeightDeform<i32> {
-    fn parse(input: &[u8], bone_idx_size: usize, weightDeformType: u8) -> IResult<&[u8], Self > {
-        match weightDeformType {
-            0 => {
-                do_parse!(input,
-                    bones: count_fixed!(BoneIndexWeight::<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, false), 1) >>
+    /// TODO: wait for const generics(RFC 2000) to revise
+    fn parse(input: &[u8], bone_idx_size: usize, weight_deform_type: u8) -> IResult<&[u8], Self> {
+        match weight_deform_type {
+            0 => do_parse!(input,
+                    bones: count_fixed!(BoneIndexWeight<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, false), 1) >>
                     (WeightDeform::BDEF1 { bones })
-                )
-            },
-            1 => {
-                do_parse!(input,
-                    bones: count_fixed!(BoneIndexWeight::<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, true), 2) >>
+                ),
+            1 => do_parse!(input,
+                    bones: count_fixed!(BoneIndexWeight<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, true), 2) >>
                     (WeightDeform::BDEF2 { bones })
-                )
-            },
-            2 => {
-                do_parse!(input,
-                    bones: count_fixed!(BoneIndexWeight::<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, true), 4) >>
+                ),
+            2 => do_parse!(input,
+                    bones: count_fixed!(BoneIndexWeight<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, true), 4) >>
                     (WeightDeform::BDEF4 { bones })
-                )
-            },
-            3 => {
-                do_parse!(input,
-                    bones: count_fixed!(BoneIndexWeight::<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, true), 2) >>
+                ),
+            3 => do_parse!(input,
+                    bones: count_fixed!(BoneIndexWeight<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, true), 2) >>
                     c: call!(Vec3::parse) >>
                     r0: call!(Vec3::parse) >>
                     r1: call!(Vec3::parse) >>
                     (WeightDeform::SDEF { bones, c, r0, r1 })
-                )
-            },
-            4 => {
-                do_parse!(input,
-                    bones: count_fixed!(BoneIndexWeight::<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, true), 4) >>
+                ),
+            4 => do_parse!(input,
+                    bones: count_fixed!(BoneIndexWeight<i32>, call!(BoneIndexWeight::<i32>::parse, bone_idx_size, true), 4) >>
                     (WeightDeform::QDEF { bones })
-                )
-            },
-            _ => unreachable!()
+                ),
+            _ => unreachable!(),
         }
     }
 }
@@ -232,36 +234,93 @@ mod tests {
     }
 
     #[test]
-    fn test_BoneIndexWeight() {
+    fn test_bone_index_weight() {
         let data = [10u8, 0u8, 0u8, 0u8, 0x40u8];
-        let(_, index_weight) = BoneIndexWeight::<i32>::parse(&data, 1, true).unwrap();
-        assert_eq!(index_weight, BoneIndexWeight::<i32> { index: 10, weight: 2.0 });
+        let (_, index_weight) = BoneIndexWeight::<i32>::parse(&data, 1, true).unwrap();
+        assert_eq!(
+            index_weight,
+            BoneIndexWeight::<i32> {
+                index: 10,
+                weight: 2.0,
+            }
+        );
     }
 
     #[test]
     fn test_header() {
-    /*
-        let head_pattern = [0x50u8, 0x4Du8, 0x58u8, 0x20u8, // b"PMX "
-                            0x00u8, 0x00u8, 0x00u8, 0x40u8, // 2.0f32 8u8
-                            0x08u8, 0x00u8, 0x00u8, 0x02u8,
-                            0x01u8, 0x01u8, 0x02u8, 0x02u8,
-                            0x02u8];
-        let r = parse_header(&head_pattern);
-        let (_, header) = r.unwrap();
-                            */
-        use std::mem;
-        let data = get_test_bytes();
-        let (_, header) = Header::parse(&data[..mem::size_of::<Header>()]).unwrap();
-        assert_eq!(header,
-           Header {
-               version: 2.0,
-               globals: Globals::from_slice(&[0x00u8, 0x00u8, 0x02u8, 0x01u8, 0x01u8,
-                                              0x02u8, 0x02u8, 0x02u8]),
-               model_name: String::from("江風"),
-               model_name_en: String::from("Model Name"),
-               comments: String::from("江風\r\n\r\nモデル制作：cham"),
-               comments_en: String::from("Comment"),
-           });
+        use encoding::EncoderTrap;
+        let mut head_pattern = vec![
+            0x50u8,
+            0x4Du8,
+            0x58u8,
+            0x20u8, // b"PMX "
+            0x00u8,
+            0x00u8,
+            0x00u8,
+            0x40u8, // 2.0f32 8u8
+            0x08u8,
+            0x00u8,
+            0x00u8,
+            0x02u8,
+            0x01u8,
+            0x01u8,
+            0x02u8,
+            0x02u8,
+            0x02u8,
+        ];
+        let model_name = String::from("モデル名前");
+        let model_name_en = String::from("Model Name");
+        let comment = String::from("コメント");
+        let comment_en = String::from("Comment");
+
+        let model_name_utf16 = UTF_16LE.encode(&model_name, EncoderTrap::Strict).unwrap();
+        let model_name_en_utf16 = UTF_16LE
+            .encode(&model_name_en, EncoderTrap::Strict)
+            .unwrap();
+        let comment_utf16 = UTF_16LE.encode(&comment, EncoderTrap::Strict).unwrap();
+        let comment_en_utf16 = UTF_16LE.encode(&comment_en, EncoderTrap::Strict).unwrap();
+        head_pattern.push(model_name_utf16.len() as u8);
+        head_pattern.push(0u8);
+        head_pattern.push(0u8);
+        head_pattern.push(0u8);
+        head_pattern.extend_from_slice(&model_name_utf16);
+        head_pattern.push(model_name_en_utf16.len() as u8);
+        head_pattern.push(0u8);
+        head_pattern.push(0u8);
+        head_pattern.push(0u8);
+        head_pattern.extend_from_slice(&model_name_en_utf16);
+        head_pattern.push(comment_utf16.len() as u8);
+        head_pattern.push(0u8);
+        head_pattern.push(0u8);
+        head_pattern.push(0u8);
+        head_pattern.extend_from_slice(&comment_utf16);
+        head_pattern.push(comment_en_utf16.len() as u8);
+        head_pattern.push(0u8);
+        head_pattern.push(0u8);
+        head_pattern.push(0u8);
+        head_pattern.extend_from_slice(&comment_en_utf16);
+        let (_, header) = Header::parse(&head_pattern).unwrap();
+
+        assert_eq!(
+            header,
+            Header {
+                version: 2.0,
+                globals: Globals::from(&[
+                    0x00u8,
+                    0x00u8,
+                    0x02u8,
+                    0x01u8,
+                    0x01u8,
+                    0x02u8,
+                    0x02u8,
+                    0x02u8
+                ]),
+                model_name,
+                model_name_en,
+                comment,
+                comment_en,
+            }
+        );
     }
 
     #[test]
@@ -269,19 +328,53 @@ mod tests {
         use std::mem;
         let pos = Vec3::unit_z();
         let normal = Vec3::unit_y();
-        let uv =  Vec2::unit_x();
+        let uv = Vec2::unit_x();
         let additional = vec![Vec4::unit_w()];
         let weight_deform = WeightDeform::BDEF1 {
-                bones: [BoneIndexWeight::<i32> { index: 99, weight: 1.0 }]
+            bones: [
+                BoneIndexWeight::<i32> {
+                    index: 99,
+                    weight: 1.0,
+                },
+            ],
         };
         let edge_scale = 9.9f32;
 
-        let data_f = unsafe { [pos.x, pos.y, pos.z, normal.x, normal.y, normal.z, uv.x, uv.y, additional[0].x, additional[0].y, additional[0].z, additional[0].w, mem::transmute(99i32 << 8) , mem::transmute((99i32 >> 24) + mem::transmute::<f32, i32>(edge_scale) << 8), {let x = mem::transmute::<f32, u32>(edge_scale); mem::transmute(x >> 24)}] };
+        let data_f = unsafe {
+            [
+                pos.x,
+                pos.y,
+                pos.z,
+                normal.x,
+                normal.y,
+                normal.z,
+                uv.x,
+                uv.y,
+                additional[0].x,
+                additional[0].y,
+                additional[0].z,
+                additional[0].w,
+                mem::transmute(99i32 << 8),
+                mem::transmute((99i32 >> 24) + mem::transmute::<f32, i32>(edge_scale) << 8),
+                {
+                    let x = mem::transmute::<f32, u32>(edge_scale);
+                    mem::transmute(x >> 24)
+                },
+            ]
+        };
         let data: [u8; 60] = unsafe { mem::transmute(data_f) };
 
         let (_, vertex) = Vertex::parse(&data, 1, 4).unwrap();
-        debug_assert_eq!(vertex,
-            Vertex { pos, normal, uv, additional, weight_deform, edge_scale }
-            );
+        debug_assert_eq!(
+            vertex,
+            Vertex {
+                pos,
+                normal,
+                uv,
+                additional,
+                weight_deform,
+                edge_scale,
+            }
+        );
     }
 }
